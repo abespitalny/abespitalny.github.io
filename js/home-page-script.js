8bd1dbf87e9a8a4ca2411df9a29796f91540d7f4
Abraham <abespitalny@gmail.com>
2018-11-27 09:47:54 -0500

    Added basic bg image randomness - Should put images in cloud and store metadata of images in database

--- js/home-page-script.js
+++ js/home-page-script.js
@@ -5,7 +5,21 @@ document.addEventListener("DOMContentLoaded", function(e) {
     // specified in Unicode
     const CURSOR_CHAR = "\u2588";
     const TYPE_TIME = 180;
+    const ARTSY_PHOTO_URLS = ["A Decadent Girl, Casas.jpg", "Abend_im_Moor_fritz_overbeck.jpg", "apple - Harold_Edgerton.png", 
+                              "banksy-butterfly-girl-suicide-stencil.jpg", "bus_on_the_open_road.jpg", "Caspar_David_Friedrich_-_Meeresstrand_im_Nebel.jpg",
+                              "Caspar_David_Friedrich-Wanderer_above_the_sea_of_fog.jpg", "Das_Eismeer - Caspar_David_Friedrich.jpg",
+                              "Eiche_im_Schnee_-_Caspar_David_Friedrich.jpg", "freewheelin_bob_dylan.jpg", "Happiness, Steve Cutts.jpg",
+                              "Melancholy, Edvard Munch.jpg", "milk_drop - Harold_Edgerton.jpg", "Newton, William Blake.jpg", "observatory-time-the-lovers-man-ray.jpg",
+                              "one_of_these_days - megatruh.jpeg", "Photo of Joan Fontaine, 1951.jpg", "Pygmalion and Galatea, Gerome.jpg", "Rock n' Roll, Sagmeister & Walsh.jpg",
+                              "Separation, Edvard Munch.jpg", "sunday_morning - jonas_de_ro.jpeg", "Train Smoke, Edvard Munch.jpg", "Vulture_on_a_Spade - Caspar_Friedrich.jpg",
+                              "Weeping Nude, Edvard Munch.jpg", "Workers on their Way Home, Edvard Munch.jpg"];
     
+    // set artsy photo
+    (function() {
+        document.getElementById("artsy-pic-bg").style.backgroundImage = `url('images/${ARTSY_PHOTO_URLS[Math.floor(ARTSY_PHOTO_URLS.length * Math.random())]}')`;
+    })();
+    
+    // create type effect
     (async function() {        
         async function timeoutPromise(time) {
             return new Promise((resolve, reject) => {
